cfe51b8503cfb6d9eac305363dc488d516b8f97f
shetimaj <kayslay@gmail.com>
2015-05-28 19:36:30 -0400

    Added data science python scripts

---
+++ scrip/s4v1.py
@@ -0,0 +1,9 @@
+import csv
+
+from s3v3 import *
+
+def write_to_file(filename, data_sample):
+  example = csv.writer(open(filename, 'w', encoding='utf-8'), dialect='excel')
+  example.writerows(data_sample)
+
+write_to_file("_data/s4-silk_ties.csv", silk_ties)
